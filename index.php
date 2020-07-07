0cfd7be66fcf45398ed1518bbc798cb193b28593
Julio Amoretti Almeyda <julio302318@gmail.com>
2020-07-07 14:25:46 -0500

    cambio total

--- index.php
+++ index.php
@@ -1,147 +1,331 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
     <title>Cacao Smart Trade</title>
-    <link rel="stylesheet" href="admin/style.css">
-    <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
-      <!-- ICONO -->
-    <link rel="shortcut icon" type="image/png" href="public/logosolo.png" />
-    <!-- FONT AWESOME -->
-    <link rel="stylesheet" href="libs/fontawesome-free/css/all.min.css">
-    <!-- FONTS -->
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet"> 
-</head>
-<body>  
-    <?php
-        require_once 'views/header.php';
-    ?>
-    
-    <main id="main">
-        <!-- VIDEO -->
-        <section class="container-gif">
-        </section>
+    <!--  ICONO -->
+    <link rel="shortcut icon" type="image/png" href="public/logosolo.png">
 
-        <section class="bg-1 pb-5">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-            <div class="container d-flex justify-content-center ">
-                <div class="text-center w-50">
-                    <img src="public/logonombre.png" alt="" class="w-50">
-                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt earum sint tempora nulla adipisci ipsum dolores officiis quibusdam! Soluta necessitatibus id adipisci laborum a aut iusto praesentium libero cumque mollitia.</h3>
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+          <!-- Cacao Smart Trade -->
+          <img src="public/logosolo.png" alt="" style="width:30px">
+          <p class="d-inline align-middle">Cacao Smart Trade</p>
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Portafolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong> Cacao Smart Trade</strong>
+              <!-- <img src="public/logonombre.png" alt=""> -->
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">
+              Somos una consultoría conformada por profesionales emprendedores altamente capacitados con experiencia en estudios de mercados, operatividad logística internacional y aduanera, brindándole la flexibilidad y confiabilidad en la búsqueda de nuevos mercados internacionales. 
+            </p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Saber más</a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">Misión</h2>
+            <hr class="light my-4">
+            <p class="text-faded mb-4">
+              Promover nacional e internacionalmente productos peruanos derivados del cacao con valor agregado, tomando desde un punto de vista analítico, con criterios socialmente responsable y ambientalmente amigable. Posteriormente realizando la exportación de su producto desde el país de origen hasta el almacén del país destino. 
+            </p>
+            <h2 class="section-heading text-white">Visión</h2>
+            <hr class="light my-4">
+            <p class="text-faded mb-4">
+            Consolidarnos como una empresa líder en exportaciones de productos no tradicionales del sector cacao, generando más puestos laborales y mejorando la economía del país. 
+            </p>
+            <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Empezar!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Nuestros Servicios</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img1.png" class="w-100 sr-icon-1">
+              <h3 class="mb-3">Derivados</h3>
+              <!-- <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p> -->
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img2.png" class="w-100 sr-icon-2">
+              <h3 class="mb-3">Emprendimiento</h3>
+              <!-- <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p> -->
+            </div>
+          </div>
+          
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img3.png" class="w-100 sr-icon-3">
+
+              <h3 class="mb-3">Fertilizantes y Plaguicidas</h3>
+              <!-- <p class="text-muted mb-0">We update dependencies to keep things fresh.</p> -->
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img4.png" class="w-100 sr-icon-4">
+
+              <h3 class="mb-3">Herramientas</h3>
+              <!-- <p class="text-muted mb-0">You have to make your websites with love these days!</p> -->
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img5.png" class="w-100 sr-icon-5">
+
+              <h3 class="mb-3">Maquinaria</h3>
+              <!-- <p class="text-muted mb-0">You have to make your websites with love these days!</p> -->
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img6.png" class="w-100 sr-icon-6">
+
+              <h3 class="mb-3">Materia Prima</h3>
+              <!-- <p class="text-muted mb-0">You have to make your websites with love these days!</p> -->
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img7.png" class="w-100 sr-icon-7">
+
+              <h3 class="mb-3">Packing</h3>
+              <!-- <p class="text-muted mb-0">You have to make your websites with love these days!</p> -->
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <img src="public/servicios/img8.png" class="w-100 sr-icon-8">
+
+              <h3 class="mb-3">Asesoramiento</h3>
+              <!-- <p class="text-muted mb-0">You have to make your websites with love these days!</p> -->
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="public/servicios2/bg1.png">
+              <img class="img-fluid" src="public/servicios2/bg1.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Planeamiento Estrategico
+                  </div>
                 </div>
-            </div>
-
-            <!-- CARDS MISION Y VISION -->
-            <div class="container ">
-
-                <div class="row justify-content-md-center">
-                    <!-- MISION -->
-                    <div class="col-xl-3  col-lg-6 col-md-6 col-sm-6  ">
-
-                        <div class="card  " >
-                            <!-- <img src="..." class="card-img-top" alt="..."> -->
-                            <div class="card-header p-3 pt-4">
-                                <h1 class="card-title text-center font-weight-bold">MISIÓN</h1>
-                            </div>
-                            <div class="card-body">
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    
-                    <!-- VISION -->
-                    <div class="col-xl-3  col-lg-6 col-md-6 col-sm-6  ">
-
-                        <div class="card  " >
-                            <!-- <img src="..." class="card-img-top" alt="..."> -->
-                            <div class="card-header p-3 pt-4">
-                                <h1 class="card-title text-center font-weight-bold">VISIÓN</h1>
-                            </div>
-                            <div class="card-body">
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-
-                    </div>
-
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="public/servicios2/bg2.png">
+              <img class="img-fluid" src="public/servicios2/bg2.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Situación Actual
+                  </div>
                 </div>
-
-            </div>
-
-
-        </section>
-
-
-       
-       
-
-        <section class="bg-2 pb-4">
-
-            <div class="container pt-4 pb-4">
-                <h1 class="text-center text-light font-weight-bold">Nuestros Productos</h1>
-            </div>
-
-            <div class="container">
-                <div class="row justify-content-md-center text-white">
-
-
-                        <div class=" col-xl-3 col-lg-3 col-md-3 col-sm-3  text-center " >
-                            <div class="p-5">
-                                <i class="fas fa-mug-hot fa-5x "></i>
-                            </div>
-                            <h6 class="font-weight-bold">Licor de cacao</h6>
-                        </div>
-
-                        <div class=" col-xl-3 col-lg-3 col-md-3 col-sm-3  text-center" >
-                            <div class="p-5">
-                                <i class="fas fa-cookie fa-5x"></i>
-                            </div>
-                            <h6 class="font-weight-bold">Tableta de cacao</h6>
-                        </div>
-
-                        <div class=" col-xl-3 col-lg-3 col-md-3 col-sm-3 text-center" >
-                            <div class="p-5">
-                                <i class="fas fa-mug-hot fa-5x"></i>
-                            </div>
-                            <h6 class="font-weight-bold">Polvo de cacao</h6>
-                        </div>
-
-
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="public/servicios2/bg3.png">
+              <img class="img-fluid" src="public/servicios2/bg3.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Selcción de mercados
+                  </div>
                 </div>
-            </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="public/servicios2/bg4.png">
+              <img class="img-fluid" src="public/servicios2/bg4.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Posicionamiento
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="public/servicios2/bg5.png">
+              <img class="img-fluid" src="public/servicios2/bg5.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Marketing Internacional
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="public/servicios2/bg6.png">
+              <img class="img-fluid" src="public/servicios2/bg6.png" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Plan Logístico
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
 
+    <!-- <section class="bg-dark text-white">
+      <div class="container text-center">
+        <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
+        <a class="btn btn-light btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Download Now!</a>
+      </div>
+    </section> -->
 
-        </section> 
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Contáctanos!</h2>
+            <hr class="my-4">
+            <p class="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <i class="fas fa-phone fa-3x mb-3 sr-contact-1"></i>
+            <p>956 999999</p>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            <i class="fas fa-envelope fa-3x mb-3 sr-contact-2"></i>
+            <p>
+              <a href="mailto:cacao@gmail.com">cacao@gmail.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
 
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.js"></script>
 
-    </main>
+  </body>
 
-
-
-
-    <div class="loader"></div>
-
-
-    <script src="libs/jquery/jquery.3.2.1.min.js"></script>
-    <script src="libs/bootstrap/bootstrap.min.js"></script>
-
-    <script>
-
-        $(document).ready(function(){
-
-            setTimeout(()=>{
-                $(".loader").fadeOut("slow");
-
-            },1500);
-
-
-        });
-
-    </script>
-
-</body>
 </html>
